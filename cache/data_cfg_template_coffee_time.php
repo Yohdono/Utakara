18f0427bae24c8be6d9b0a8a4e69217e41187ddc
unknown <yang@.(none)>
2011-01-07 16:36:47 +0100

    nettoyage

--- cache/data_cfg_template_coffee_time.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1325723077
-181
-a:5:{s:4:"name";s:11:"coffee_time";s:9:"copyright";s:31:"&copy; CoSa NoStrA DeSiGn, 2008";s:7:"version";s:5:"3.0.6";s:17:"template_bitfield";s:4:"lNg=";s:8:"filetime";i:1259105104;}
--- cache/sql_ce16d28237dd9ad3973e07a9280c4bb8.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1294415839
-SELECT m.*, u.user_colour, g.group_colour, g.group_type FROM (Uta_moderator_cache m) LEFT JOIN Uta_users u ON (m.user_id = u.user_id) LEFT JOIN Uta_groups g ON (m.group_id = g.group_id) WHERE m.display_on_index = 1
-6
-a:0:{}
